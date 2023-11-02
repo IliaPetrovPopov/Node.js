b7ccea03e6c4e36df56dcfb4eb9894fc56f8cf9d
Ilia Popov <ilia_popov02@abv.bg>
2023-11-02 18:24:08 +0200

    Add main file

---
+++ index.js
@@ -0,0 +1,85 @@
+import fs from 'fs';
+import http from 'http';
+import url from 'url';
+import { replaceTemplate } from './services/template.service.mjs';
+import slugify from 'slugify';
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocade: ${textIn}.\n
+// Created on ${Date.now()} `;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2} ${data3}`,
+//         "utf-8",
+//         (err) => {
+//             console.log("Your file has been written");
+//         }
+//       );
+//     });
+//   });
+// });
+
+const tempOverview = fs.readFileSync(
+  `./templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(`./templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(
+  `./templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`./dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  } else {
+    res
+      .writeHead(404, {
+        'Content-type': 'text/html',
+      })
+      .end('<h1>Page not found</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
+});
